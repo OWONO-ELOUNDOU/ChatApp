5fdf7d1e80db66e2caa97f25c195b339ee8d9665
OWONO ELOUNDOU <OWONO ELOUNDOU>
2023-01-03 00:24:06 +0100

    firebase services added

--- Fire.js
+++ Fire.js
@@ -31,8 +31,43 @@ class Fire {
         messages.forEach(item => {
             const message = {
                 text: item.text,
-                timestamp: firebase.database.ServerValue.TIMESTAMP
-            }
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            };
+
+            this.db.push(message);
         })
+    };
+
+    parse = message => {
+        const {user, test, timestamp} = message.val()
+        const {key: _id} = message
+        const createdAt = new Date(timestamp)
+
+        return {
+            _id,
+            createdAt,
+            text,
+            user
+        }
+    };
+
+    get = callback => {
+        this.db.on("child_added", snapshot => callback(this.parse(snapshot)));
+    };
+
+    off() {
+        this.db.off();
+    };
+
+    get db() {
+        return firebase.database().ref("messages")
+    };
+
+    get uid() {
+        return (firebase.auth().currentUser || {}).uid
     }
 }
+
+
+export default new Fire();
